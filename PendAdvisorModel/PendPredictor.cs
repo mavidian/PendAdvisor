1b13b113c0e3399573de62ea95deebaa6dfdf195
Wojtek Dziegielewski <wojtekd@mavidian.com>
2020-11-24 16:18:26 -0500

    Implement PendPredictor class.

--- PendAdvisorModel/PendPredictor.cs
+++ PendAdvisorModel/PendPredictor.cs
@@ -7,12 +7,20 @@ namespace PendAdvisorModel
    public static class PendPredictor
    {
       public static string PathToModelLocation { get; } = Path.GetFullPath("MLModel.zip");
-      
+
+      public static ModelOutput Predict(ModelInput input)
+      {
+         return PredictionEngine.Value.Predict(input);
+      }
+
+
       private static Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictionEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(CreatePredictionEngine);
 
       private static PredictionEngine<ModelInput, ModelOutput> CreatePredictionEngine()
       {
-         throw new NotImplementedException();
+         MLContext mlContext = new MLContext();
+         ITransformer mlModel = mlContext.Model.Load(PathToModelLocation, out _);
+         return mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
       }
 
    }
