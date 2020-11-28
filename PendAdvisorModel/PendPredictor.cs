cbdbc837cd4f93f16b2af0f1910563a72544cd82
Wojtek Dziegielewski <wojtekd@mavidian.com>
2020-11-28 16:59:45 -0500

    Friendly model output (ModelOutputEx); add PredictEx method to PendPredictor.

--- PendAdvisorModel/PendPredictor.cs
+++ PendAdvisorModel/PendPredictor.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.ML;
+using Microsoft.ML.Data;
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace PendAdvisorModel
@@ -22,13 +24,51 @@ namespace PendAdvisorModel
       }
 
 
+      /// <summary>
+      /// Cross-reference between a 0-based index to the array with Label values and the actual value in the Label column.
+      /// (does not get assigned until prediction engine is created, e.g. by calling Predict method).
+      /// </summary>
+      public static Dictionary<int, string> LabelMap { get; private set; }
+
+      public static ModelOutputEx PredictEx(ModelInput input)
+      {
+         var modelEx = (ModelOutputEx)Predict(input);
+
+         // ActionsAndScores is a set of tuples (Action and Score) sorted by Score (highest first).
+         modelEx.ActionsAndScores =
+            modelEx.Scores.Zip(Enumerable.Range(0, int.MaxValue), (f, s) => (First: f, Second: s))  // First = Score, Second = index
+                        .OrderByDescending(t => t.First)
+                        .Select(t => (Action: GetLabelMap(PredictionEngine.Value)[t.Second], Score: t.First))
+                        .ToArray();
+         return modelEx;
+      }
+
+
       private static Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictionEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(CreatePredictionEngine);
 
       private static PredictionEngine<ModelInput, ModelOutput> CreatePredictionEngine()
       {
          MLContext mlContext = new MLContext();
          ITransformer mlModel = mlContext.Model.Load(PathToModelLocation, out _);
-         return mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+         var predictor = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+         LabelMap = GetLabelMap(predictor);  //translates 0-based index to the actual value in the Label column.
+         return predictor;
+      }
+
+
+      /// <summary>
+      /// Calculate cross-reference between a 0-based index to the array with Label values and the actual value in the Label column.
+      /// </summary>
+      /// <param name="predictor">Predicting engine (has the index to prediction mapping defined during MapValueToKey conversion).</param>
+      /// <returns></returns>
+      private static Dictionary<int, string> GetLabelMap(PredictionEngine<ModelInput, ModelOutput> predictor)
+      {  //inspired by https://blog.hompus.nl/2020/09/14/get-all-prediction-scores-from-your-ml-net-model/
+         var labelBuffer = new VBuffer<ReadOnlyMemory<char>>();
+         predictor.OutputSchema["Score"].Annotations.GetValue("SlotNames", ref labelBuffer);
+         var labels = labelBuffer.DenseValues().Select(l => l.ToString());
+
+         int i = 0;
+         return labels.ToDictionary(_ => i++, l => l);
       }
 
    }
